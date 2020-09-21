a9bde996d5775d4be4d2f2b4bd02854c94eda822
Lars <lsoraas@gmail.com>
2020-09-21 22:36:21 +0200

    Create srv.py

---
+++ srv.py
@@ -0,0 +1,15 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/celcius/<value>')
+def calculateF(value):
+    return value + " Celcius er " + str(float(value)*1.8 + 32) + " i Fahrenheit"
+
+@app.route('/fahrenheit/<value>')
+def calculateC(value):
+    return value + " Fahrenheit er " + str((float(value)/1.8) - 32) + " i Celcius"
+    
+if __name__ == "__main__":
+    app.run(debug=True)
+    
